2cb960713eba68354c8f2b2b58410281d5dc3d1b
July58 <perevozniukjv@gmail.com>
2022-10-18 08:50:04 +0300

    commit

---
+++ app/src/main/java/com/example/lab2/OutputFragment.java
@@ -0,0 +1,40 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+public class OutputFragment extends Fragment {
+
+    private TextView textView3;
+    private Button cancel;
+
+
+       @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+       View v = inflater.inflate(R.layout.fragment_output, container, false);
+       cancel = v.findViewById(R.id.cancel);
+       textView3 = v.findViewById(R.id.textView3);
+
+      cancel.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               textView3.setText("");
+               ((RadioGroup)getActivity().findViewById(R.id.radios)).clearCheck();
+               ((EditText)getActivity().findViewById(R.id.user_field)).setText("");
+           }
+       });
+
+    return v;}
+}
